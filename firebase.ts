f5b0b7b1fd591601a3a6f65bf3a26a830b10fe42
celstintech-maker <celstintech@gmail.com>
2026-01-30 01:28:44 +0100

    feat: Add Firebase analytics integration
    Integrates Google Analytics for Firebase into the Omni Marketplace application. This will enable tracking of user interactions and application usage.

--- firebase.ts
+++ firebase.ts
@@ -1,24 +1,27 @@
 
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Omni Marketplace Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGGabk52HnE2uE4zTvoGtgggMT8YaRb6o",
-  authDomain: "omnimarketplace-live.firebaseapp.com",
-  projectId: "omnimarketplace-live",
-  storageBucket: "omnimarketplace-live.firebasestorage.app",
-  messagingSenderId: "373197878288",
-  appId: "1:373197878288:web:c8b4361a6e8f4cc0adc695",
-  measurementId: "G-5MMW8732EE"
+  apiKey: "AIzaSyA6_Yj9aD9gzuP-Ye6YqofO6mX-ilhVi-U",
+  authDomain: "omni-marketplace-c34ab.firebaseapp.com",
+  projectId: "omni-marketplace-c34ab",
+  storageBucket: "omni-marketplace-c34ab.firebasestorage.app",
+  messagingSenderId: "280702093091",
+  appId: "1:280702093091:web:5a76f228b449ce41e1ee96",
+  measurementId: "G-BY16RDQN72"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Services
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
+export { analytics };
 export default app;
